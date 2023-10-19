af8031aca0e8ef917a2b72d3b18a3ad3ee2d7ff5
Maximiliano Vilariño <maxi.vilarino@gmail.com>
2023-10-19 11:24:20 +0200

    Create Hero section and reusable Button component

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,5 +1,78 @@
+import Image from "next/image";
+import Button from "./Button";
+
 const Hero = () => {
-  return <h1>Hero</h1>;
+  return (
+    <section className="max-container padding-container flex flex-col gap-20 py-10 pb-32 md:gap-28 lg:py20 xl:flex-row">
+      <div className="hero-map" />
+      <section className="relative z-20 flex flex-col xl:w-1/2">
+        <Image
+          src="/camp.svg"
+          alt="camp"
+          width={50}
+          height={50}
+          className="absolute left-[-5px] top-[-30px] w-10 lg:w-[50px]"
+        />
+        <h1 className="bold-52 lg:bold-88">Gandario Camp Area</h1>
+        <p className="regular-16 mt-6 text-gray-30 xl:max-w-[520px]">
+          We want to be on each of your journeys seeking the satisfaction of
+          seeing the incorruptible beauty of nature. We can help you on an
+          adventure around the world in just one app
+        </p>
+        <section className="my-11 flex flex-wrap gap-5">
+          <section className="flex items-center gap-2">
+            {Array(5)
+              .fill(1)
+              .map((_, index) => (
+                <Image
+                  src="/star.svg"
+                  key={index}
+                  alt="star"
+                  width={24}
+                  height={24}
+                />
+              ))}
+          </section>
+          <p className="bold-16 lg:bold-20 text-blue-70">
+            198k
+            <span className="regular-16 lg:regular-20 ml-1">
+              Excellent Reviews
+            </span>
+          </p>
+        </section>
+        <section className="flex flex-col w-full gap-3 sm:flex-row">
+          <Button type="button" title="Download App" variant="btn_green" />
+          <Button
+            type="button"
+            title="How we work?"
+            icon="/play.svg"
+            variant="btn_white_text"
+          />
+        </section>
+      </section>
+      <section className="relative flex flex-1 items-start">
+        <section className="relative z-20 flex w-[268px] flex-col gap-8 rounded-3xl bg-green-90 px-7 py-8">
+          <section className="flex flex-col">
+            <section className="flexBetween">
+              <p className="regular-16 text-gray-20">Location</p>
+              <Image src="/close.svg" alt="close" width={24} height={24} />
+            </section>
+            <p className="bold-20 text-white">Bergondo</p>
+          </section>
+          <section className="flexBetween">
+            <section className="flex flex-col">
+              <p className="regular-16 block text-gray-20">Distance</p>
+              <p className="bold-20 text-white">173.28 Km</p>
+            </section>
+            <section className="flex flex-col">
+              <p className="regular-16 block text-gray-20">Elevation</p>
+              <p className="bold-20 text-white">2.040 Km</p>
+            </section>
+          </section>
+        </section>
+      </section>
+    </section>
+  );
 };
 
 export default Hero;
