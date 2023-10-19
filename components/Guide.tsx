5a5b44d9c41449eb509f0c87b790fb50dbd387e3
Maximiliano Vilariño <maxi.vilarino@gmail.com>
2023-10-19 13:14:14 +0200

    Create Guide Section

--- components/Guide.tsx
+++ components/Guide.tsx
@@ -1,5 +1,59 @@
+import Image from "next/image";
+
 const Guide = () => {
-  return <h1>Guide</h1>;
+  return (
+    <section>
+      <section className="padding-container max-container w-full pb-24">
+        <Image src="/camp.svg" alt="camp" width={50} height={50} />
+        <p className="uppercase regular-18 -mt-1 mb-3 text-green-50">
+          We are here for you
+        </p>
+        <section className="flex flex-wrap justify-between gap-5 lg:gap-10">
+          <h2 className="bold-40 lg:bold-64 xl:max-w-[390px]">
+            Guide You to Easy Path
+          </h2>
+          <p className="regular-16 text-gray-30 xl:max-w-[520px]">
+            Only with the hilink application you will no longer get lost and get
+            lost again, because we already support offline maps when there is no
+            internet connection in the field. Invite your friends, relatives and
+            friends to have fun in the wilderness through the valley and reach
+            the top of the mountain
+          </p>
+        </section>
+      </section>
+      <section className="flexCenter max-container relative w-full">
+        <Image
+          src="/boat.png"
+          alt="boat"
+          width={1440}
+          height={580}
+          className="w-full object-cover object-center 2xl:rounded-5xl"
+        />
+        <section className="absolute flex bg-white py-8 pl-5 pr-7 gap-3 rounded-3xl border shadow-md md:left-[5%] lg:top-20">
+          <Image
+            src="/meter.svg"
+            alt="meter"
+            width={16}
+            height={158}
+            className="h-full w-auto"
+          />
+          <section className="flexBetween flex-col">
+            <section className="flex w-full flex-col">
+              <section className="flexBetween w-full">
+                <p className="regular-16 text-gray-20">Destination</p>
+                <p className="bold-16 text-green-50">48 min</p>
+              </section>
+              <p className="bold-20 mt-2">Bergondo</p>
+            </section>
+            <section className="flex w-full flex-col">
+              <p className="regular-16 text-gray-20">Start track</p>
+              <h4 className="bold-20 mt-2 whitespace-nowrap">A Coruña</h4>
+            </section>
+          </section>
+        </section>
+      </section>
+    </section>
+  );
 };
 
 export default Guide;
